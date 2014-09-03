aefb8ec250ed48cb56050cd41c2adfab57c12021
Chris Moesel <cmoesel@mitre.org>
2014-09-03 17:34:44 -0400

    Remove legacy CQL model code

--- Src/java/elm/src/main/java/org/cqframework/cql/elm/Main.java
+++
@@ -1,7 +0,0 @@
-package org.cqframework.cql.elm;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
--- Src/java/poc/translator/src/main/java/org/cqframework/cql/poc/translator/expressions/Statement.java
+++
@@ -1,7 +0,0 @@
-package org.cqframework.cql.poc.translator.expressions;
-
-/**
- * Created by bobd on 7/25/14.
- */
-public abstract class Statement extends Expression {
-}
