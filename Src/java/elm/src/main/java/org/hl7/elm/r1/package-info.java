77857eb19a67f6478566d8b11de883ae0a664867
Bryn Rhodes <tangiblezero@gmail.com>
2014-09-10 08:32:15 -0600

    Added elm package-info to set namespaces for serialization. Correct fix is to use a NamespacePrefixMapping, but this will work with the hardcoded namespaces for now.

---
+++ Src/java/elm/src/main/java/org/hl7/elm/r1/package-info.java
@@ -0,0 +1,10 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:hl7-org:elm:r1", xmlns = {
+        @XmlNs(prefix = "", namespaceURI = "urn:hl7-org:elm:r1"),
+        @XmlNs(prefix = "xsi", namespaceURI = "http://www.w3.org/2001/XMLSchema-instance"),
+        @XmlNs(prefix = "xsd", namespaceURI = "http://www.w3.org/2001/XMLSchema"),
+        @XmlNs(prefix = "quick", namespaceURI = "http://org.hl7.fhir"),
+        @XmlNs(prefix = "cqla", namespaceURI = "urn:hl7-org:cql-annotations:r1")},
+        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.hl7.elm.r1;
+
+import javax.xml.bind.annotation.XmlNs;
