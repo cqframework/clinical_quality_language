b0d372ed75e55ec204acdc2178e53e6711bf9da1
Jason Walonoski <jwalonoski@mitre.org>
2014-12-01 15:08:23 -0500

    Removed duplicative test. Functionality moved to Engine and EngineTest.

--- Src/java/cql-execution/src/test/java/org/cqframework/cql/execution/JavascriptEngineTest.java
+++ Src/java/cql-execution/src/test/java/org/cqframework/cql/execution/JavascriptEngineTest.java
@@ -1,15 +1,8 @@
 package org.cqframework.cql.execution;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ImporterTopLevel;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
-import org.mozilla.javascript.commonjs.module.Require;
-import org.mozilla.javascript.tools.shell.Global;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -71,52 +64,5 @@ public class JavascriptEngineTest {
 //		Engine.dump();
 		
 		Assert.assertEquals( Engine.getLastResults().results.size(), TestPatientSource.maxPatients);		
-	}
-	
-	@Test
-	public void testJavascriptRequire()
-	{
-		Context context = Context.enter();
-		ScriptableObject scope = context.initStandardObjects();
-		
-		Global global = new Global(context);
-		boolean sandboxed = false;
-		List<String> modulePath = new ArrayList<String>();
-		String mainModule = "../../coffeescript/cql-execution/lib/";
-		modulePath.add(mainModule);
-		Require require = global.installRequire(context, modulePath, sandboxed);
-		require.install(scope);
-		
-	    Scriptable arguments = context.newArray(scope, new Object[] {});
-	    scope.defineProperty("arguments", arguments, ScriptableObject.DONTENUM);
-
-		TestPatientSource patients = new TestPatientSource();
-		patients.initialize(context, scope);
-		Engine.setPatientSource(patients);
-		Engine.reset();
-	    
-		Object result = null;
-		try {
-			File lib = new File(mainModule);
-			File script = new File( lib, "age-exec.js" );
-			
-			String uri = script.toURI().toURL().toExternalForm();
-			ScriptableObject.putProperty(scope, "moduleUri", uri);
-			
-			require.requireMain(context, "age-exec");
-		
-//			System.out.println( "Results: " + Engine.getLastResults().results.size() );
-//			System.out.flush();
-//			Engine.dump();
-			
-		} catch(Exception e) {
-			System.err.println(e.getClass().getName() + " -- " + e.getLocalizedMessage());
-			File f = new File(".");
-			System.err.println(f.getAbsolutePath());
-			Assert.fail(e.getLocalizedMessage());
-		}
-		Context.exit();
-
-		Assert.assertNotNull( Context.toString(result) );	
 	}
 }
