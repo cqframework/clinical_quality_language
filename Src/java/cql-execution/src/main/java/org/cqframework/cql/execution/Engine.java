8d6b3d69e8c4fbc4c20cbb200186aedc5f6f6349
Jason Walonoski <jwalonoski@mitre.org>
2014-12-01 12:02:17 -0500

    Added javadoc.

--- Src/java/cql-execution/src/main/java/org/cqframework/cql/execution/Engine.java
+++ Src/java/cql-execution/src/main/java/org/cqframework/cql/execution/Engine.java
@@ -17,33 +17,57 @@ public class Engine {
 	private static Results results = new Results();
 	private static PatientSource patientSource;
 	
+	/** 
+	 * Set the PatientSource to be used by all CQL scripts.
+	 * If the PatientSource is {@code null}, script execution
+	 * will fail and throw an Exception.
+	 */
 	public static void setPatientSource(PatientSource source) 
 	{
 		patientSource = source;		
 	}
 	
+	/** Get the current PatientSource being used by all CQL scripts. */
 	public static PatientSource getPatientSource()
 	{
 		return patientSource;
 	}
 	
+	/**
+	 * Adds an object to the currently executing CQL scripts result set.
+	 * Should be called from the CQL (Javascript) only.
+	 */
 	public static void add(Object object) {
 		results.add(object);
 	}
 	
+	/** Dump the contents of the results to standard out. */
 	public static void dump() {
 		results.dump();
 	}
 	
+	/** Clear the result set and reset the patient source. */
 	public static void reset() {
 		results.clear();
-		patientSource.reset();
+		if(patientSource!=null) {
+			patientSource.reset();			
+		}
 	}
 	
+	/** 
+	 * Gets a copy of result set from the last CQL script
+	 * that was executed.
+	 */
 	public static Results getLastResults() {
 		return results.copy();
 	}
 
+	/**
+	 * Execute a CQL script.
+	 * @param file File containing the full CQL script ("includes" are not allowed).
+	 * @return The result set of the execution.
+	 * @throws Exception if the patient source is {@code null}.
+	 */
 	public static Results executeCql(File file) throws Exception
 	{
 		CqlTranslator rosetta = CqlTranslator.fromFile(file);
@@ -51,6 +75,12 @@ public class Engine {
 		return execute(json);
 	}
 	
+	/**
+	 * Execute a CQL script.
+	 * @param cql String containing the full CQL script ("includes" are not allowed).
+	 * @return The result set of the execution.
+	 * @throws Exception if the patient source is {@code null}.
+	 */
 	public static Results executeCql(String cql) throws Exception
 	{
 		CqlTranslator rosetta = CqlTranslator.fromText(cql);
@@ -58,11 +88,24 @@ public class Engine {
 		return execute(json);
 	}
 	
+	/**
+	 * Execute a JSON expression or JavaScript script.
+	 * @param json The full script to execute.
+	 * @return The result set of the execution.
+	 * @throws Exception if the patient source is {@code null},
+	 * even if the script doesn't actually use it.
+	 */
 	public static Results executeJson(String json) throws Exception
 	{
 		return execute(json);
 	}
 	
+	/**
+	 * Execute JavaScript representing a CQL measure in the Rhino engine.
+	 * @param json The script to execute.
+	 * @return The result set of the execution.
+	 * @throws Exception if the patient source is {@code null}.
+	 */
 	private static Results execute(String json) throws Exception
 	{
 		if(patientSource == null) {
--- Src/java/cql-execution/src/main/java/org/cqframework/cql/execution/PatientSource.java
+++ Src/java/cql-execution/src/main/java/org/cqframework/cql/execution/PatientSource.java
@@ -16,10 +16,20 @@ public interface PatientSource
 {
 	/**
 	 * Initialize the PatientSource with the JavaScript context and scope.
+	 * This is necessary so that the PatientSource can correctly create,
+	 * scope, and return NativeObjects with the shift method.
 	 */
 	public void initialize(Context context, Scriptable scope);
 	
 	/**
+	 * Returns the next patient as NativeObject JSON.
+	 * 
+	 * The method name, "shift", was specifically selected to mirror
+	 * the JavaScript Array.shift method, so that a PatientSource in
+	 * the JavaScript can successfully interface with a Java-based
+	 * implementation or a JavaScript Array-based implementation
+	 * (useful for testing).
+	 * 
 	 * @return The next patient as NativeObject JSON.
 	 */
 	public NativeObject shift();
