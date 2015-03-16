a29eaea06bfd36d0f286e43d27e6cc6c725d306f
Bryn Rhodes <tangiblezero@gmail.com>
2015-03-16 10:24:56 -0600

    Changed Pos to PositionOf.

--- Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/SystemLibraryHelper.java
+++ Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/SystemLibraryHelper.java
@@ -173,7 +173,7 @@ public class SystemLibraryHelper {
         system.add(new Operator("Indexer", new Signature(systemModel.getString(), systemModel.getInteger()), systemModel.getString()));
         system.add(new Operator("Length", new Signature(systemModel.getString()), systemModel.getInteger()));
         system.add(new Operator("Lower", new Signature(systemModel.getString()), systemModel.getString()));
-        system.add(new Operator("Pos", new Signature(systemModel.getString(), systemModel.getString()), systemModel.getInteger()));
+        system.add(new Operator("PositionOf", new Signature(systemModel.getString(), systemModel.getString()), systemModel.getInteger()));
         system.add(new Operator("Split", new Signature(systemModel.getString(), systemModel.getString()), new ListType(systemModel.getString())));
         system.add(new Operator("Substring", new Signature(systemModel.getString(), systemModel.getInteger()), systemModel.getString()));
         system.add(new Operator("Substring", new Signature(systemModel.getString(), systemModel.getInteger(), systemModel.getInteger()), systemModel.getString()));
