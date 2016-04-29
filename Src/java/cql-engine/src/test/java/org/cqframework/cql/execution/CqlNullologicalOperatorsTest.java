7df94133089fb7633b9bf290809999b275fcc9c2
Jeff Swenson <jeffongit@gmail.com>
2016-04-29 10:20:35 -0600

    Adding CqlExecutionTestBase
    Refactored common code to base class.

--- Src/java/cql-engine/src/test/java/org/cqframework/cql/execution/CqlNullologicalOperatorsTest.java
+++ Src/java/cql-engine/src/test/java/org/cqframework/cql/execution/CqlNullologicalOperatorsTest.java
@@ -1,55 +1,17 @@
 package org.cqframework.cql.execution;
 
-import org.cqframework.cql.cql2elm.CqlTranslator;
-import org.cqframework.cql.cql2elm.LibraryManager;
 import org.cqframework.cql.elm.execution.Library;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBException;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URLDecoder;
-import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class CqlNullologicalOperatorsTest {
-    private static final String fileName = CqlNullologicalOperatorsTest.class.getSimpleName();
-    private static File xmlFile = null;
-
-    @BeforeClass
-    public static void oneTimeSetUp() {
-        ArrayList<CqlTranslator.Options> options = new ArrayList<>();
-        options.add(CqlTranslator.Options.EnableDateRangeOptimization);
-
-        try {
-            File cqlFile = new File(URLDecoder.decode(CqlNullologicalOperatorsTest.class.getResource(fileName + ".cql").getFile(),"UTF-8"));
-            LibraryManager libraryManager = new LibraryManager();
-            CqlTranslator translator = CqlTranslator.fromFile(cqlFile, libraryManager, options.toArray(new CqlTranslator.Options[options.size()]));
-
-            xmlFile = File.createTempFile(fileName, ".xml");
-            try (PrintWriter pw = new PrintWriter(xmlFile, "UTF-8")) {
-                pw.println(translator.toXml());
-                pw.println();
-                pw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @AfterClass
-    public static void oneTimeTearDown() {
-        if (xmlFile != null) {
-            xmlFile.delete();
-        }
+public class CqlNullologicalOperatorsTest extends CqlExecutionTestBase {
+    static {
+        testClass = CqlNullologicalOperatorsTest.class;
     }
 
     @Test
