6d6baa5c0ae63bf3084ac12b7960276694ee4b1f
Jeff Swenson <jeffongit@gmail.com>
2016-05-02 11:01:02 -0600

    Adding Scaffolding for Type Opperators

--- Src/java/cql-engine/src/test/java/org/cqframework/cql/execution/CqlTypeOperatorsTest.java
+++ Src/java/cql-engine/src/test/java/org/cqframework/cql/execution/CqlTypeOperatorsTest.java
@@ -7,9 +7,59 @@ import javax.xml.bind.JAXBException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class CqlTypeOperatorsTest  extends CqlExecutionTestBase {
+public class CqlTypeOperatorsTest extends CqlExecutionTestBase {
     static {
         testClass = CqlTypeOperatorsTest.class;
+    }
+
+    @Test
+    public void testAS() throws JAXBException {
+
+    }
+
+    @Test
+    public void testConvert() throws JAXBException {
+
+    }
+
+    @Test
+    public void testIs() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToBoolean() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToConcept() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToDateTime() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToDecimal() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToInteger() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToQuantity() throws JAXBException {
+
+    }
+
+    @Test
+    public void testToString() throws JAXBException {
+        
     }
 
     @Test
