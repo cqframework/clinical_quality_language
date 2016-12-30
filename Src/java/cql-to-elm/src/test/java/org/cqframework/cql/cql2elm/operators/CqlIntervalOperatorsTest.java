0a43c97eec58f67a7cd464a6c46e56f397deccab
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2016-12-30 15:31:26 -0700

    Fixed an issue with incorrect generic signature instantiation resolution.

---
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/operators/CqlIntervalOperatorsTest.java
@@ -0,0 +1,51 @@
+package org.cqframework.cql.cql2elm.operators;
+
+import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.LibraryManager;
+import org.cqframework.cql.cql2elm.ModelManager;
+import org.hl7.elm.r1.After;
+import org.hl7.elm.r1.Equal;
+import org.hl7.elm.r1.ExpressionDef;
+import org.hl7.elm.r1.Library;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.cqframework.cql.cql2elm.matchers.HasTypeAndResult.hasTypeAndResult;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by Bryn on 12/30/2016.
+ */
+public class CqlIntervalOperatorsTest {
+
+    // NOTE: The CQL for this test is taken from an engine testing suite that produced a particular issue with generic instantiations.
+    // This library will not translate successfully without the proper fix in place.
+    // So this test only needs to validate that the library translates successfully.
+
+    private Map<String, ExpressionDef> defs;
+
+    @BeforeTest
+    public void setup() throws IOException {
+        ModelManager modelManager = new ModelManager();
+        CqlTranslator translator = CqlTranslator.fromStream(CqlIntervalOperatorsTest.class.getResourceAsStream("../OperatorTests/CqlIntervalOperators.cql"), modelManager, new LibraryManager(modelManager));
+        assertThat(translator.getErrors().size(), is(0));
+        Library library = translator.toELM();
+        defs = new HashMap<>();
+        if (library.getStatements() != null) {
+            for (ExpressionDef def : library.getStatements().getDef()) {
+                defs.put(def.getName(), def);
+            }
+        }
+    }
+
+    @Test
+    public void testAfter() {
+        ExpressionDef def = defs.get("TestAfterNull");
+        assertThat(def, hasTypeAndResult(After.class, "System.Boolean"));
+    }
+}
