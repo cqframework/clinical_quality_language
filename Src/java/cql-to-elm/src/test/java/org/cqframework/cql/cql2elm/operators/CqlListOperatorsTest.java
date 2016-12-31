b83cce60eec7f5ad4ce17df0ed9e89e338133605
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2016-12-31 16:09:30 -0700

    Fixed an issue with union type determination incorrectly resulting in a choice type.

---
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/operators/CqlListOperatorsTest.java
@@ -0,0 +1,45 @@
+package org.cqframework.cql.cql2elm.operators;
+
+import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.LibraryManager;
+import org.cqframework.cql.cql2elm.ModelManager;
+import org.hl7.elm.r1.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.cqframework.cql.cql2elm.matchers.HasTypeAndResult.hasTypeAndResult;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by Bryn on 12/30/2016.
+ */
+public class CqlListOperatorsTest {
+    // NOTE: The CQL for this test is taken from an engine testing suite that produced a particular issue with ambiguous conversions.
+
+    private Map<String, ExpressionDef> defs;
+
+    @BeforeTest
+    public void setup() throws IOException {
+        ModelManager modelManager = new ModelManager();
+        CqlTranslator translator = CqlTranslator.fromStream(CqlListOperatorsTest.class.getResourceAsStream("../OperatorTests/CqlListOperators.cql"), modelManager, new LibraryManager(modelManager));
+        assertThat(translator.getErrors().size(), is(0));
+        Library library = translator.toELM();
+        defs = new HashMap<>();
+        if (library.getStatements() != null) {
+            for (ExpressionDef def : library.getStatements().getDef()) {
+                defs.put(def.getName(), def);
+            }
+        }
+    }
+
+    @Test
+    public void testUnion() {
+        ExpressionDef def = defs.get("Union123AndEmpty");
+        assertThat(def, hasTypeAndResult(Union.class, "list<System.Any>"));
+    }
+}
