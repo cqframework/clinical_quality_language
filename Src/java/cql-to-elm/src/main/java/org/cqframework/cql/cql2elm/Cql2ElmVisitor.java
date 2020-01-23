3f74e0fb6397f7ca0e668016204e7361510bea0f
Jonathan Percival <jonathan.i.percival@gmail.com>
2020-01-23 08:44:37 -0700

    Sort directions

--- Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/Cql2ElmVisitor.java
+++ Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/Cql2ElmVisitor.java
@@ -3395,11 +3395,7 @@ DATETIME
 
     @Override
     public SortDirection visitSortDirection(@NotNull cqlParser.SortDirectionContext ctx) {
-        if (ctx.getText().equals("desc")) {
-            return SortDirection.DESC;
-        }
-
-        return SortDirection.ASC;
+        return SortDirection.fromValue(ctx.getText());
     }
 
     private SortDirection parseSortDirection(cqlParser.SortDirectionContext ctx) {
