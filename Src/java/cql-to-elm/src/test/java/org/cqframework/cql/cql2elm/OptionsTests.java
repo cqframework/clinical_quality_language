1923ccaf3a50e9a18e43bf8dc7a1f103cd964f33
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2020-04-22 08:22:22 -0600

    #517: Added CqlTranslatorOptionsMapper class to encapsulate reading/writing options.

--- Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/OptionsTests.java
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/OptionsTests.java
@@ -1,6 +1,5 @@
 package org.cqframework.cql.cql2elm;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,18 +13,17 @@ public class OptionsTests {
     @Test
     public void testTranslatorOptions() throws IOException {
         CqlTranslatorOptions options = CqlTranslatorOptions.defaultOptions();
-        ObjectMapper om = new ObjectMapper();
         StringWriter sw = new StringWriter();
-        om.writeValue(sw, options);
+        CqlTranslatorOptionsMapper.toWriter(sw, options);
         String result = sw.toString();
         Assert.assertTrue(result != null);
 
         InputStream input = OptionsTests.class.getResourceAsStream("options.json");
-        CqlTranslatorOptions readOptions = om.readValue(new InputStreamReader(input), CqlTranslatorOptions.class);
+        CqlTranslatorOptions readOptions = CqlTranslatorOptionsMapper.fromReader(new InputStreamReader(input));
         Assert.assertTrue(readOptions != null);
 
         StringWriter sw2 = new StringWriter();
-        om.writeValue(sw2, readOptions);
+        CqlTranslatorOptionsMapper.toWriter(sw2, readOptions);
         String result2 = sw2.toString();
         Assert.assertTrue(result.equals(result2));
     }
