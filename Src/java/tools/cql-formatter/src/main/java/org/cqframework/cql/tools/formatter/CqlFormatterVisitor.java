f3d51a9b0e5a1a7abbbd7195f795a820d6ad7171
Evan Chicoine <evan.chicoine@esacinc.com>
2021-09-20 15:46:53 -0400

    #613: Fixed prepend logic for spacing on keyword starts

--- Src/java/tools/cql-formatter/src/main/java/org/cqframework/cql/tools/formatter/CqlFormatterVisitor.java
+++ Src/java/tools/cql-formatter/src/main/java/org/cqframework/cql/tools/formatter/CqlFormatterVisitor.java
@@ -219,6 +219,7 @@ public class CqlFormatterVisitor extends cqlBaseVisitor<Object> {
             case ")": return !inFunctionDefinition() && !inFunctionInvocation();
             case "[": return inRetrieve();
             case "]": return false;
+            case "starts": return !inFunctionDefinition() || !inFunctionInvocation();
             default: return true;
         }
     }
@@ -229,7 +230,7 @@ public class CqlFormatterVisitor extends cqlBaseVisitor<Object> {
             case "<": return !inTypeSpecifier();
             case ">": return !inTypeSpecifier();
             case "(": return !inFunctionDefinition() && !inFunctionInvocation();
-            case ")": return !inFunctionDefinition() && !inFunctionInvocation();
+            case ")": return !inFunctionDefinition() || !inFunctionInvocation();
             case "[": return false;
             case "]": return inRetrieve();
             default: return true;
