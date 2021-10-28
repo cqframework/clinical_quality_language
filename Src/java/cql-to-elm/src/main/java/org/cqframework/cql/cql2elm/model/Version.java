f3591dd9baece50eb1ae29a6d688bb898b4061dd
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2021-10-28 11:15:38 -0600

    support alphanumeric in version specifier

--- Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/Version.java
+++ Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/Version.java
@@ -12,7 +12,7 @@ public class Version implements Comparable<Version> {
     public Version(String version) {
         if(version == null)
             throw new IllegalArgumentException("Version can not be null");
-        if(!version.matches("[0-9]+(\\.[0-9]+)*"))
+        if(!version.matches("[0-9A-Za-z]+(\\.[0-9A-Za-z]+)*"))
             throw new IllegalArgumentException("Invalid version format");
         this.version = version;
     }
