1199b8411c3804d2967629f074c979efaee935df
Vitor Pamplona <vitor@vitorpamplona.com>
2022-06-28 13:21:18 -0400

    Reducing XML files by not exporting null objects.

--- Src/java/elm-jackson/src/main/java/org/cqframework/cql/elm/serializing/jackson/ElmXmlMapper.java
+++ Src/java/elm-jackson/src/main/java/org/cqframework/cql/elm/serializing/jackson/ElmXmlMapper.java
@@ -1,5 +1,6 @@
 package org.cqframework.cql.elm.serializing.jackson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -20,6 +21,7 @@ public class ElmXmlMapper {
             .enable(SerializationFeature.INDENT_OUTPUT)
             .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
             .enable(MapperFeature.USE_BASE_TYPE_AS_DEFAULT_IMPL)
+            .defaultPropertyInclusion(JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL))
             .addModule(new JaxbAnnotationModule())
             //.addMixIn(TypeInfo.class, TypeInfoMixIn.class)
             .addMixIn(TypeSpecifier.class, TypeSpecifierMixIn.class)
