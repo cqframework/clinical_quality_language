b500018a01e94237cb7f6847d47cf10520d99d36
Vitor Pamplona <vitor@vitorpamplona.com>
2022-07-11 12:04:29 -0400

    Fixing #768

--- Src/java/model-jackson/src/main/java/org/hl7/elm_modelinfo/r1/serializing/jackson/XmlModelInfoReader.java
+++ Src/java/model-jackson/src/main/java/org/hl7/elm_modelinfo/r1/serializing/jackson/XmlModelInfoReader.java
@@ -1,8 +1,11 @@
 package org.hl7.elm_modelinfo.r1.serializing.jackson;
 
+import com.ctc.wstx.stax.WstxInputFactory;
+import com.ctc.wstx.stax.WstxOutputFactory;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
@@ -18,7 +21,11 @@ import java.net.URI;
 import java.net.URL;
 
 public class XmlModelInfoReader implements ModelInfoReader {
-    static XmlMapper mapper = XmlMapper.builder()
+    static XmlMapper mapper = XmlMapper
+            // TODO: Remove new XmlFactory(new WstxInputFactory(), new WstxOutputFactory()) in the future when Android
+            //  receives an implementation of javax.xml.stream.XMLInputFactory.newFactory ('xml-apis:xml-apis:1.4.01')
+            // Fixes #768: Forces the use of Woodstock for Android users.
+            .builder(new XmlFactory(new WstxInputFactory(), new WstxOutputFactory()))
             .defaultUseWrapper(false)
             .defaultMergeable(true)
             .enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION)
