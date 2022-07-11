ad1b5bbb2ea076f47d8fd0864ced2d470658a9d2
Vitor Pamplona <vitor@vitorpamplona.com>
2022-07-11 14:47:44 -0400

    Fixes #768: Forces Woodstox on all Jackson modules (#769)
    Fixing #768

--- Src/java/elm-jackson/src/main/java/org/cqframework/cql/elm/serializing/jackson/ElmXmlMapper.java
+++ Src/java/elm-jackson/src/main/java/org/cqframework/cql/elm/serializing/jackson/ElmXmlMapper.java
@@ -1,9 +1,12 @@
 package org.cqframework.cql.elm.serializing.jackson;
 
+import com.ctc.wstx.stax.WstxInputFactory;
+import com.ctc.wstx.stax.WstxOutputFactory;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
@@ -13,7 +16,11 @@ import org.hl7.cql_annotations.r1.CqlToElmBase;
 import org.hl7.elm.r1.TypeSpecifier;
 
 public class ElmXmlMapper {
-    private static XmlMapper mapper = XmlMapper.builder()
+    private static XmlMapper mapper = XmlMapper
+            // TODO: Remove new XmlFactory(new WstxInputFactory(), new WstxOutputFactory()) in the future when Android
+            //  receives an implementation of javax.xml.stream.XMLInputFactory.newFactory ('xml-apis:xml-apis:1.4.01')
+            // Fixes #768: Forces the use of Woodstock for Android users.
+            .builder(new XmlFactory(new WstxInputFactory(), new WstxOutputFactory()))
             .defaultUseWrapper(true)
             .defaultMergeable(true)
             .enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION)
