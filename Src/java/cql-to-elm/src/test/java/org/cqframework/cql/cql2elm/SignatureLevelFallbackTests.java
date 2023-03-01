237bd891380e1998e69c43e4bfd3cb074f807fe5
mdnazmulkarim <nazmul.karim@smilecdr.com>
2023-02-28 17:00:54 -0700

    add test data

---
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/SignatureLevelFallbackTests.java
@@ -0,0 +1,48 @@
+package org.cqframework.cql.cql2elm;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.hl7.elm.r1.Library;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SignatureLevelFallbackTests {
+
+    ModelManager modelManager;
+    LibraryManager libraryManager;
+
+    @BeforeClass
+    public void setup() {
+        modelManager = new ModelManager();
+        libraryManager = new LibraryManager(modelManager);
+        libraryManager.getLibrarySourceLoader().registerProvider(new TestLibrarySourceProvider());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        libraryManager.getLibrarySourceLoader().clearProviders();
+    }
+
+    @Test
+    public void testLibraryReferences() {
+        try {
+            CqlCompiler compiler = null;
+            libraryManager = new LibraryManager(modelManager);
+            libraryManager.getLibrarySourceLoader().registerProvider(new TestLibrarySourceProvider());
+            compiler = new CqlCompiler(modelManager, libraryManager);
+            Library library = compiler.run(LibraryTests.class.getResourceAsStream("LibraryTests/SignatureLevelFallback.cql"),
+                    CqlCompilerException.ErrorSeverity.Info);
+
+            assertThat(compiler.getErrors().size(), is(0));
+            System.out.println(CqlTranslator.convertToJson(library));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
