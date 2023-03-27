e1d6aa28a9374b85bd247f4acfba4a059792c3b3
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2023-03-27 09:48:11 -0600

    clean ups

--- Src/java/engine-jackson/src/test/java/org/opencds/cqf/cql/engine/execution/CqlContextTest.java
+++ Src/java/engine-jackson/src/test/java/org/opencds/cqf/cql/engine/execution/CqlContextTest.java
@@ -1,43 +1,36 @@
 package org.opencds.cqf.cql.engine.execution;
 
 import org.cqframework.cql.elm.execution.Library;
-import org.fhir.ucum.UcumException;
 import org.testng.annotations.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.time.ZonedDateTime;
-import java.util.TimeZone;
 
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.is;
-//import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class CqlContextTest {
 
-//    @Test
-//    public void test_simpleLibrary_returnsResult() throws IOException, UcumException {
-//        Library library = translate("portable/CqlOverloadingTestSuite.cql");
-//        Context context = new Context(library, ZonedDateTime.of(2018, 1, 1, 7, 0, 0, 0, TimeZone.getDefault().toZoneId()));
-//        Object result = context.resolveExpressionRef("TestAnyFunctionWithInteger").getExpression().evaluate(context);
-//        System.out.println("result:" + result);
-//    }
-
     @Test
-    public void test_method_overload_signature_none() throws IOException {
+    public void test_method_overload_with_no_signature() throws IOException {
         TranslatorHelper translatorHelper = new TranslatorHelper();
         Library library = translatorHelper.readJson(translatorHelper.readFromInputStream(CqlContextTest.class.getResourceAsStream("methodOverloadSingatureNone.json")));
-        //Library library = translatorHelper.readJson(translatorHelper.readFromInputStream(CqlContextTest.class.getResourceAsStream("methodOverloadSingatureOverload.json")));
-        Context context = new Context(library, ZonedDateTime.of(2018, 1, 1, 7, 0, 0, 0, TimeZone.getDefault().toZoneId()));
+        Context context = new Context(library);
         Object result = context.resolveExpressionRef("TestAnyFunctionWithInteger").getExpression().evaluate(context);
-        System.out.println("result:" + result);
+        assertThat(result, is(1));
+
+        result = context.resolveExpressionRef("TestAnyFunctionWithString").getExpression().evaluate(context);
+        assertThat(result, is("joe"));
     }
 
-    private static Library translate(String file)  throws UcumException, IOException {
-        return new TranslatorHelper().translate(file);
+    @Test
+    public void test_method_overload_with_signature() throws IOException {
+        TranslatorHelper translatorHelper = new TranslatorHelper();
+        Library library = translatorHelper.readJson(translatorHelper.readFromInputStream(CqlContextTest.class.getResourceAsStream("methodOverloadSingatureOverload.json")));
+        Context context = new Context(library);
+        Object result = context.resolveExpressionRef("TestAnyFunctionWithInteger").getExpression().evaluate(context);
+        assertThat(result, is(1));
+
+        result = context.resolveExpressionRef("TestAnyFunctionWithString").getExpression().evaluate(context);
+        assertThat(result, is("joe"));
     }
+
 }
