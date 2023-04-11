e9527055122c5984dd532f6c84669285927f4f6e
c-schuler <hoofschu@gmail.com>
2023-04-11 11:57:21 -0600

    Implemented logging, improved reporting (error and debug), duplication checks, and enhanced testing

--- Src/java/cqf-fhir-npm/src/test/java/org/cqframework/fhir/npm/NpmPackageManagerTests.java
+++ Src/java/cqf-fhir-npm/src/test/java/org/cqframework/fhir/npm/NpmPackageManagerTests.java
@@ -4,7 +4,6 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import ca.uhn.fhir.context.FhirContext;
@@ -18,105 +17,46 @@ import org.hl7.fhir.r5.context.IWorkerContext;
 import org.hl7.fhir.r5.model.ImplementationGuide;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class NpmPackageManagerTests implements IWorkerContext.ILoggingService {
 
-    private Logger logger = LoggerFactory.getLogger(NpmPackageManagerTests.class);
-    private VersionConvertor_40_50 convertor = new VersionConvertor_40_50(new BaseAdvisor_40_50());
-    /*
-    NOTE: This test depends on the dev package cache for the [sample-ig](https://github.com/FHIR/sample-ig)
-    Running the IG publisher on a clone of this IG locally will create and cache the package
-
-    TODO: Update this so that it can download and cache packages by itself
-     */
-    @Ignore("Currently requires running the IG Publisher first to cache npm packages.")
-    @Test
-    public void TestSampleIG() throws IOException {
-        NpmPackageManager pm = NpmPackageManager.fromStream(NpmPackageManagerTests.class.getResourceAsStream("myig.xml"), "4.0.1");
-        assertTrue(pm.getNpmList().size() >= 1);
-    }
+    private final Logger logger = LoggerFactory.getLogger(NpmPackageManagerTests.class);
+    private final VersionConvertor_40_50 convertor = new VersionConvertor_40_50(new BaseAdvisor_40_50());
 
     @Test
-    public void TestSampleIGLocal() throws IOException {
+    public void TestSampleIGLocal() {
         Resource igResource = (Resource) FhirContext.forR4Cached().newXmlParser().parseResource(
                 NpmPackageManagerTests.class.getResourceAsStream("myig.xml"));
         ImplementationGuide ig = (ImplementationGuide) convertor.convertResource(igResource);
-        NewNpmPackageManager pm = new NewNpmPackageManager(ig);
-        assertTrue(pm.getNpmList().size() >= 1);
-    }
-
-    /*
-    NOTE: This test depends on the dev package cache for the [sample-content-ig](https://github.com/cqframework/sample-content-ig)
-    Running the IG publisher on a clone of this IG locally will create and cache the package
-
-    NOTE: Temporarily @Ignore because it's causing the CI build to fail.
-    TODO: Update this so that it can download and cache packages by itself
-     */
-    @Ignore("Currently requires running the IG Publisher first to cache npm packages.")
-    @Test
-    public void TestSampleContentIG() throws IOException {
-        NpmPackageManager pm = NpmPackageManager.fromStream(NpmPackageManagerTests.class.getResourceAsStream("mycontentig.xml"), "4.0.1");
-        assertTrue(pm.getNpmList().size() >= 1);
+        NpmPackageManager pm = new NpmPackageManager(ig);
+        assertEquals(pm.getNpmList().size(), 1);
     }
 
     @Test
-    public void TestSampleContentIGLocal() throws IOException {
+    public void TestSampleContentIGLocal() {
         Resource igResource = (Resource) FhirContext.forR4Cached().newXmlParser().parseResource(
                 NpmPackageManagerTests.class.getResourceAsStream("mycontentig.xml"));
         ImplementationGuide ig = (ImplementationGuide) convertor.convertResource(igResource);
-        NewNpmPackageManager pm = new NewNpmPackageManager(ig);
-        assertTrue(pm.getNpmList().size() >= 1);
-    }
-
-    /*
-    Ignoring this test because even though it passes locally in all reviewers environments, it fails when running in the Travis build,
-    best guess is that the Travis environment doesn't allow access to the cache directory for some reason.
-     */
-    @Ignore
-    @Test
-    public void TestOpioidMMEIG() throws IOException {
-        NpmPackageManager pm = NpmPackageManager.fromStream(NpmPackageManagerTests.class.getResourceAsStream("opioid-mme-r4.xml"), "4.0.1");
-        assertTrue(pm.getNpmList().size() >= 1);
+        NpmPackageManager pm = new NpmPackageManager(ig);
+        assertEquals(pm.getNpmList().size(), 3);
     }
 
     @Test
-    public void TestOpioidMMEIGLocal() throws IOException {
+    public void TestOpioidMMEIGLocal() {
         Resource igResource = (Resource) FhirContext.forR4Cached().newXmlParser().parseResource(
                 NpmPackageManagerTests.class.getResourceAsStream("opioid-mme-r4.xml"));
         ImplementationGuide ig = (ImplementationGuide) convertor.convertResource(igResource);
-        NewNpmPackageManager pm = new NewNpmPackageManager(ig);
-        assertTrue(pm.getNpmList().size() >= 1);
-    }
-
-    /*
-    NOTE: This test depends on the dev package cache for the [sample-content-ig](https://github.com/cqframework/sample-content-ig)
-    Running the IG publisher on a clone of this IG locally will create and cache the package
-
-    NOTE: Temporarily @Ignore because it's causing the CI build to fail.
-     */
-    @Ignore
-    @Test
-    public void TestLibrarySourceProvider() throws IOException {
-        NpmPackageManager pm = NpmPackageManager.fromStream(NpmPackageManagerTests.class.getResourceAsStream("mycontentig.xml"), "4.0.1");
-        assertTrue(pm.getNpmList().size() >= 1);
-
-        LibraryLoader reader = new LibraryLoader("4.0.1");
-        NpmLibrarySourceProvider sp = new NpmLibrarySourceProvider(pm.getNpmList(), reader, this);
-        InputStream is = sp.getLibrarySource(new VersionedIdentifier().withSystem("http://somewhere.org/fhir/uv/myig").withId("example"));
-        assertNotNull(is);
-        is = sp.getLibrarySource(new VersionedIdentifier().withSystem("http://somewhere.org/fhir/uv/myig").withId("example").withVersion("0.2.0"));
-        assertNotNull(is);
+        NpmPackageManager pm = new NpmPackageManager(ig);
+        assertEquals(pm.getNpmList().size(), 2);
     }
 
     @Test
-    public void TestLibrarySourceProviderLocal() throws IOException {
+    public void TestLibrarySourceProviderLocal() {
         Resource igResource = (Resource) FhirContext.forR4Cached().newXmlParser().parseResource(
                 NpmPackageManagerTests.class.getResourceAsStream("mycontentig.xml"));
         ImplementationGuide ig = (ImplementationGuide) convertor.convertResource(igResource);
-        NewNpmPackageManager pm = new NewNpmPackageManager(ig);
-        assertTrue(pm.getNpmList().size() >= 1);
+        NpmPackageManager pm = new NpmPackageManager(ig);
 
         LibraryLoader reader = new LibraryLoader("4.0.1");
         NpmLibrarySourceProvider sp = new NpmLibrarySourceProvider(pm.getNpmList(), reader, this);
@@ -126,31 +66,12 @@ public class NpmPackageManagerTests implements IWorkerContext.ILoggingService {
         assertNotNull(is);
     }
 
-    /*
-    Ignoring this test because even though it passes locally in all reviewers environments, it fails when running in the Travis build,
-    best guess is that the Travis environment doesn't allow access to the cache directory for some reason.
-     */
-    @Ignore
     @Test
-    public void TestModelInfoProvider() throws IOException {
-        InputStream is = NpmPackageManagerTests.class.getResourceAsStream("testig.xml");
-        assertNotNull(is);
-        NpmPackageManager pm = NpmPackageManager.fromStream(is, "4.0.1");
-        assertTrue(pm.getNpmList().size() >= 1);
-
-        LibraryLoader reader = new LibraryLoader("4.0.1");
-        NpmModelInfoProvider mp = new NpmModelInfoProvider(pm.getNpmList(), reader, this);
-        ModelInfo mi = mp.load(new ModelIdentifier().withSystem("http://hl7.org/fhir/us/qicore").withId("QICore"));
-        assertNotNull(mi);
-        assertEquals(mi.getName(), "QICore");
-    }
-
-    @Test
-    public void TestModelInfoProviderLocal() throws IOException {
+    public void TestModelInfoProviderLocal() {
         Resource igResource = (Resource) FhirContext.forR4Cached().newXmlParser().parseResource(
                 NpmPackageManagerTests.class.getResourceAsStream("testig.xml"));
         ImplementationGuide ig = (ImplementationGuide) convertor.convertResource(igResource);
-        NewNpmPackageManager pm = new NewNpmPackageManager(ig);
+        NpmPackageManager pm = new NpmPackageManager(ig);
         assertTrue(pm.getNpmList().size() >= 1);
 
         LibraryLoader reader = new LibraryLoader("5.0");
