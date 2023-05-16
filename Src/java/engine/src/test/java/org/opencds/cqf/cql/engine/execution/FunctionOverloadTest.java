dd92cefaef0ef0266ab92dc7a98da698e4b37900
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2023-05-16 13:39:50 -0600

    fix function overload test

---
+++ Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/execution/FunctionOverloadTest.java
@@ -0,0 +1,33 @@
+package org.opencds.cqf.cql.engine.execution;
+
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FunctionOverloadTest extends CqlTestBase {
+
+    @Test
+    public void test_all_empty_string() {
+        EvaluationResult evaluationResult;
+
+        evaluationResult = engineVisitor.evaluate(toElmIdentifier("FunctionOverloadTest"), null, null, null, null, null);
+
+        Object result;
+
+        result = evaluationResult.expressionResults.get("TestAnyFunctionWithInteger").value();
+        assertThat(result, is(1));
+
+        result = evaluationResult.expressionResults.get("TestAnyFunctionWithString").value();
+        assertThat(result, is("joe"));
+
+        result = evaluationResult.expressionResults.get("TestAnyFunctionWithDecimal").value();
+        assertThat(result, is(new BigDecimal("12.3")));
+
+        result = evaluationResult.expressionResults.get("TestAnyFunctionWithNoArgs").value();
+        assertThat(result, is("any"));
+
+    }
+}
