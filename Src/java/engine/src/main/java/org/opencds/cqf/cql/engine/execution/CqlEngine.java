f34324aac5a7a888936e92d50558cc246b938c9b
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2023-06-15 14:17:21 -0600

    update narrative

--- Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/CqlEngine.java
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/CqlEngine.java
@@ -26,24 +26,10 @@ import static org.opencds.cqf.cql.engine.execution.NamespaceHelper.getNamePart;
 import static org.opencds.cqf.cql.engine.execution.NamespaceHelper.getUriPart;
 
 /**
- * NOTE: Several possible approaches to traversing the ELM tree for execution:
- * <p>
- * 1. "Executable" Node Hierarchy: Create nodes for each ELM type and deserialize into these nodes
- * This option works well, but is problematic for maintenance because Java doesn't have partial classes.
- * There also doesn't seem to be a way to tell JAXB which hierarchy to use if you have two different hierarchies
- * for the same schema (Trackable-based ELM used by the CQL-to-ELM translator, and Executable-based ELM used by the engine).
- * This could potentially be a bonus though, as it forces the engine to not take a dependency on the translator, forcing
- * a clean separation between the translator and the engine.
- * <p>
- * 2. Visitor Pattern: This option is potentially simpler to implement, however:
- * a. The visitor pattern doesn't lend itself well to aggregation of results, which is the real work of each node anyway
- * b. Extensibility is compromised, difficult to introduce new nodes (unlikely to be a practical issue though)
- * c. Without lambdas, the cost of traversal is quite high due to the expensive if-then-else chains in the visitor nodes
- * <p>
- * So, opting for the Executable Node Hierarchy for now, knowing that it creates a potential maintenance issue, but
- * this is acceptable because the ELM Hierarchy is settling down, and so long as all the non-generated code is at the
- * end, this should be easy to maintain. In addition, it will be much more performant, and lend itself much better to
- * the aggregation of values from child nodes.
+ * NOTE: We have updated CqlEngine adopt a visitor pattern approach to traversing the ELM tree for execution:
+ *
+ * Visitor pattern reduces the process to convert EML Tree to Executable ELM tree and thus reduces a potential maintenance issue.
+ *
  */
 
 public class CqlEngine extends ElmBaseLibraryVisitor<Object, State> {
