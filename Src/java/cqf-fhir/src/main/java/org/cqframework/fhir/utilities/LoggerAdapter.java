2d799aeae53d617eef41be3f45b6a74882b489ab
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-08-18 15:14:18 -0600

    Push NPM upstream

---
+++ Src/java/cqf-fhir/src/main/java/org/cqframework/fhir/utilities/LoggerAdapter.java
@@ -0,0 +1,27 @@
+package org.cqframework.fhir.utilities;
+
+import org.hl7.fhir.r5.context.IWorkerContext;
+import org.slf4j.Logger;
+
+public class LoggerAdapter implements IWorkerContext.ILoggingService {
+  private Logger innerLogger;
+
+  public LoggerAdapter(Logger innerLogger) {
+    this.innerLogger = innerLogger;
+  }
+
+  @Override
+  public void logMessage(String s) {
+    innerLogger.info(s);
+  }
+
+  @Override
+  public void logDebugMessage(LogCategory logCategory, String s) {
+    innerLogger.debug("{}: {}", logCategory, s);
+  }
+
+  @Override
+  public boolean isDebugLogging() {
+    return this.innerLogger.isDebugEnabled();
+  }
+}
