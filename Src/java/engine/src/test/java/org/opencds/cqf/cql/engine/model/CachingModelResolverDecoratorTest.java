2a962c3773df7aa680ed8096e0adfb53ac7bec34
JP <jonathan.i.percival@gmail.com>
2023-08-22 15:20:14 -0600

    Upstream caching model resolver (#1197)

---
+++ Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/model/CachingModelResolverDecoratorTest.java
@@ -0,0 +1,28 @@
+package org.opencds.cqf.cql.engine.model;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+// TODO: Extend testing to cover more of the CachedModelResolver
+public class CachingModelResolverDecoratorTest {
+
+
+    @Test
+    public void context_path_resolved_only_once() {
+        var m = mock(ModelResolver.class);
+        when(m.getContextPath("Patient", "Patient")).thenReturn("id");
+
+        var cache = new CachingModelResolverDecorator(m);
+        cache.getContextPath("Patient", "Patient");
+        var result = cache.getContextPath("Patient", "Patient");
+
+        assertEquals("id", result);
+        verify(m, times(1)).getContextPath("Patient", "Patient");
+    }
+}
