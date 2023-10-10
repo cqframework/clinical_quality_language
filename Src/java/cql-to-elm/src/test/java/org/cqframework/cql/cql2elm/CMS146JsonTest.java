ec61bccc13b4d9b6566e23f8620a86b3915556dd
Luke deGruchy <luke.degruchy@smilecdr.com>
2023-10-10 10:57:33 -0400

    Fix CMS146JsonTest to properly verify all signature level cases.

--- Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/CMS146JsonTest.java
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/CMS146JsonTest.java
@@ -2,11 +2,15 @@ package org.cqframework.cql.cql2elm;
 
 import org.cqframework.cql.cql2elm.CqlCompilerException.ErrorSeverity;
 import org.cqframework.cql.cql2elm.LibraryBuilder.SignatureLevel;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,62 +18,39 @@ import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
 
 public class CMS146JsonTest {
 
-    private static final String SIG_LEVEL_TOKEN = "{SIGNATURE_LEVEL_TOKEN}";
+    @DataProvider(name = "sigFileAndSigLevel")
+    private static Object[][] sigFileAndSigLevel() {
+        return new Object[][] {
+                {"CMS146v2_Expected_SignatureLevel_None.json", SignatureLevel.None},
+                {"CMS146v2_Expected_SignatureLevel_Differing.json", SignatureLevel.Differing},
+                {"CMS146v2_Expected_SignatureLevel_Overloads.json", SignatureLevel.Overloads},
+                {"CMS146v2_Expected_SignatureLevel_All.json", SignatureLevel.All}
+        };
+    }
 
-    @Test
-    public void testCms146_SignatureLevel_None() throws IOException {
-        File expectedJsonFile = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Expected.json").getFile(), "UTF-8"));
-        String expectedJson = new Scanner(expectedJsonFile, "UTF-8").useDelimiter("\\Z").next().replace(SIG_LEVEL_TOKEN, SignatureLevel.None.name());
+    @Test(dataProvider = "sigFileAndSigLevel")
+    public void testCms146_SignatureLevels(String fileName, SignatureLevel expectedSignatureLevel) throws IOException {
+        final String expectedJson = getJson(fileName);
 
-        File cms146 = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Test_CQM.cql").getFile(), "UTF-8"));
-        ModelManager modelManager = new ModelManager();
-        CqlTranslator translator = CqlTranslator.fromFile(cms146, new LibraryManager(modelManager, new CqlCompilerOptions(ErrorSeverity.Warning, SignatureLevel.None)));
-        String actualJson = translator.toJson();
+        final File cms146 = getFile("CMS146v2_Test_CQM.cql");
+        final ModelManager modelManager = new ModelManager();
+        final CqlTranslator translator = CqlTranslator.fromFile(cms146, new LibraryManager(modelManager, new CqlCompilerOptions(ErrorSeverity.Warning, expectedSignatureLevel)));
+        final String actualJson = translator.toJson();
         assertThat(actualJson, sameJSONAs(expectedJson));
     }
 
-    // LUKETODO:
-    /*
-        but: library.statements.def[4].expression.relationship[1].suchThat.operand[1].operand
-        Unexpected: signature
-         ; library.statements.def[6].expression.operand.relationship[0].suchThat.operand[1].operand
-        Unexpected: signature
-         ; library.statements.def[11].expression.operand.where.operand[1].operand
-        Unexpected: signatur
-     */
-    @Test
-    public void testCms146_SignatureLevel_Differing() throws IOException {
-        File expectedJsonFile = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Expected.json").getFile(), "UTF-8"));
-        String expectedJson = new Scanner(expectedJsonFile, "UTF-8").useDelimiter("\\Z").next().replace(SIG_LEVEL_TOKEN, SignatureLevel.Differing.name());
-
-        File cms146 = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Test_CQM.cql").getFile(), "UTF-8"));
-        ModelManager modelManager = new ModelManager();
-        CqlTranslator translator = CqlTranslator.fromFile(cms146, new LibraryManager(modelManager, new CqlCompilerOptions(ErrorSeverity.Warning, SignatureLevel.Differing)));
-        String actualJson = translator.toJson();
-        assertThat(actualJson, sameJSONAs(expectedJson));
+    private static String getJson(String name) throws IOException {
+        return new Scanner(getFile(name), StandardCharsets.UTF_8)
+                .useDelimiter("\\Z").next();
     }
 
-    @Test
-    public void testCms146_SignatureLevel_Overloads() throws IOException {
-        File expectedJsonFile = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Expected.json").getFile(), "UTF-8"));
-        String expectedJson = new Scanner(expectedJsonFile, "UTF-8").useDelimiter("\\Z").next().replace(SIG_LEVEL_TOKEN, SignatureLevel.Overloads.name());
+    private static File getFile(String name) {
+        final URL resource = CMS146JsonTest.class.getResource(name);
 
-        File cms146 = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Test_CQM.cql").getFile(), "UTF-8"));
-        ModelManager modelManager = new ModelManager();
-        CqlTranslator translator = CqlTranslator.fromFile(cms146, new LibraryManager(modelManager, new CqlCompilerOptions(ErrorSeverity.Warning, SignatureLevel.Overloads)));
-        String actualJson = translator.toJson();
-        assertThat(actualJson, sameJSONAs(expectedJson));
-    }
+        if (resource == null) {
+            throw new IllegalArgumentException("Cannot find file with name: " + name);
+        }
 
-    @Test
-    public void testCms146_SignatureLevel_All() throws IOException {
-        File expectedJsonFile = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Expected.json").getFile(), "UTF-8"));
-        String expectedJson = new Scanner(expectedJsonFile, "UTF-8").useDelimiter("\\Z").next().replace(SIG_LEVEL_TOKEN, SignatureLevel.All.name());
-
-        File cms146 = new File(URLDecoder.decode(CMS146JsonTest.class.getResource("CMS146v2_Test_CQM.cql").getFile(), "UTF-8"));
-        ModelManager modelManager = new ModelManager();
-        CqlTranslator translator = CqlTranslator.fromFile(cms146, new LibraryManager(modelManager, new CqlCompilerOptions(ErrorSeverity.Warning, SignatureLevel.All)));
-        String actualJson = translator.toJson();
-        assertThat(actualJson, sameJSONAs(expectedJson));
+        return new File(URLDecoder.decode(resource.getFile(), StandardCharsets.UTF_8));
     }
 }
