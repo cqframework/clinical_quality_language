dd33b2884e42510778b012a0bb5f39aab23d8006
iasmile <inaam.ahmed@smilecdr.com>
2023-10-18 12:35:48 -0400

    chore: removed added whitespace

--- Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/SemanticTests.java
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/SemanticTests.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.testng.Assert.assertThrows;
 
-
 public class SemanticTests {
 
     @Test
