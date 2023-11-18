3d27d6beab3570eee55e1f02b857fffdcc3dac97
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-11-18 12:27:45 -0700

    Should fail

---
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/TotallyNewClass.java
@@ -0,0 +1,23 @@
+package org.opencds.cqf.cql.engine.execution;
+
+
+// This should fail code cov since it's all new code and untested
+public class TotallyNewClass {
+
+    public static String totallyNewMethod() {
+        return "Totally new method";
+    }
+
+    public static String totallyNewMethod2() {
+        return "Totally new method 2";
+    }
+
+    public String randonMeString() {
+        return "Totally new method 2";
+    }
+
+    public void whatEverwhat() {
+        System.out.println("What ever what");
+    }
+
+}
