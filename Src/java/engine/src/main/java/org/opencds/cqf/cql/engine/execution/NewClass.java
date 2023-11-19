798fe6fba630f08e5aad632ed923ff6afc6e5819
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-11-19 11:47:15 -0700

    Testing new code behavior

---
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/NewClass.java
@@ -0,0 +1,18 @@
+package org.opencds.cqf.cql.engine.execution;
+
+public class NewClass {
+
+    public void doThing() {
+        System.out.println("Hello World!");
+    }
+
+    public void doThing2() {
+
+        System.out.println("Hello Mundo!");
+    }
+
+     public void doThing3() {
+        System.out.println("What what!");
+    }
+
+}
---
+++ Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/execution/NewClassTest.java
@@ -0,0 +1,14 @@
+package org.opencds.cqf.cql.engine.execution;
+
+import org.testng.annotations.Test;
+
+public class NewClassTest {
+
+    @Test
+    public void test() {
+        NewClass newClass = new NewClass();
+        newClass.doThing();
+        newClass.doThing2();
+        newClass.doThing3();
+    }
+}
