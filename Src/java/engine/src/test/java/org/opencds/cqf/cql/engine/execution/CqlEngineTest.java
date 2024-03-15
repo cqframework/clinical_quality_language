4586863e3a94b197905590a4ec47eaf2eddec850
JP <jonathan.i.percival@gmail.com>
2024-03-15 09:02:00 -0600

    Add Debug logging for expressions (#1341)
    * Initial logging fix
    
    * Add an initial test for DebugMap
    
    * push some gated logic down

---
+++ Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlEngineTest.java
@@ -0,0 +1,38 @@
+package org.opencds.cqf.cql.engine.execution;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.opencds.cqf.cql.engine.debug.DebugMap;
+import org.testng.annotations.Test;
+
+class CqlEngineTest extends CqlTestBase {
+
+    @Test
+    void testDebugMap() {
+
+        // The specific library isn't important, just that it has a debug map
+        var debugMap = new DebugMap();
+        debugMap.setIsLoggingEnabled(true);
+        var results = engine.evaluate(toElmIdentifier("CqlEngineTest"), null, null, null, debugMap);
+
+        var libraryDebug = results.getDebugResult()
+                .getLibraryResults()
+                .get("CqlEngineTest")
+                .getResults();
+
+        assertNotNull(libraryDebug);
+
+        // Find the debug result for the AnInteger expression
+        // It's indexed by location
+        var result = libraryDebug.keySet().stream()
+                .filter(e -> e.getLocator().equals("6:1-6:21"))
+                .findFirst();
+
+        assertTrue(result.isPresent());
+
+        var debugResult = libraryDebug.get(result.get());
+        assertEquals(1, debugResult.size());
+    }
+}
