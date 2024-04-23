960c175fa6ce6e406b4eb9ca505145d6a53d5575
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-04-23 10:34:15 -0600

    Experimenting

--- Src/java/elm-jaxb/src/main/java/org/cqframework/cql/elm/serializing/jaxb/ElmJsonMapper.java
+++ Src/java/elm-jaxb/src/main/java/org/cqframework/cql/elm/serializing/jaxb/ElmJsonMapper.java
@@ -2,17 +2,23 @@ package org.cqframework.cql.elm.serializing.jaxb;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import java.util.HashMap;
 import org.hl7.cql_annotations.r1.CqlToElmBase;
 import org.hl7.elm.r1.Library;
 
 public class ElmJsonMapper {
 
     private static JAXBContext jaxbContext;
+    private static HashMap<String, String> properties = new HashMap<>();
+
+    static {
+        properties.put(JAXBContext.JAXB_CONTEXT_FACTORY, "org.eclipse.persistence.jaxb.JAXBContextFactory");
+    }
 
     public static JAXBContext getJaxbContext() {
         if (jaxbContext == null) {
             try {
-                jaxbContext = JAXBContext.newInstance(Library.class, CqlToElmBase.class);
+                jaxbContext = JAXBContext.newInstance(new Class<?>[] {Library.class, CqlToElmBase.class}, properties);
             } catch (JAXBException e) {
                 throw new RuntimeException("Error creating JAXBContext - " + e.getMessage());
             }
--- Src/java/elm-jaxb/src/main/java/org/cqframework/cql/elm/serializing/jaxb/ElmXmlMapper.java
+++ Src/java/elm-jaxb/src/main/java/org/cqframework/cql/elm/serializing/jaxb/ElmXmlMapper.java
@@ -2,17 +2,24 @@ package org.cqframework.cql.elm.serializing.jaxb;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import java.util.HashMap;
 import org.cqframework.cql.elm.IdObjectFactory;
 
 public class ElmXmlMapper {
 
     private static JAXBContext jaxbContext;
+    private static HashMap<String, String> properties = new HashMap<>();
+
+    static {
+        properties.put(JAXBContext.JAXB_CONTEXT_FACTORY, "org.eclipse.persistence.jaxb.JAXBContextFactory");
+    }
 
     public static JAXBContext getJaxbContext() {
         if (jaxbContext == null) {
             try {
-                jaxbContext =
-                        JAXBContext.newInstance(IdObjectFactory.class, org.hl7.cql_annotations.r1.ObjectFactory.class);
+                jaxbContext = JAXBContext.newInstance(
+                        new Class<?>[] {IdObjectFactory.class, org.hl7.cql_annotations.r1.ObjectFactory.class},
+                        properties);
             } catch (JAXBException e) {
                 throw new RuntimeException("Error creating JAXBContext - " + e.getMessage());
             }
