03db5ab0d3892983114eac021d38434f7af201a8
Anton Vasetenkov <antvaset@gmail.com>
2024-07-31 15:08:09 +1200

    Add tests for ProperIn

--- Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/elm/executing/ProperInEvaluator.java
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/elm/executing/ProperInEvaluator.java
@@ -6,13 +6,18 @@ package org.opencds.cqf.cql.engine.elm.executing;
         T, Interval : The type of T must be the same as the point type of the interval.
 
     For the T, List overload, this operator returns if the given element is in the given list,
-        and it is not the only element in the list, using equivalence semantics.
-        If the list-valued argument is null, it should be treated as an empty list.
+        and it is not the only element in the list, using equality semantics, with the exception
+        that null elements are considered equal.
+        If the first argument is null, the result is true if the list contains any null elements
+        and at least one other element, and false otherwise.
+        If the second argument is null, the result is false.
 
     For the T, Interval overload, this operator returns true if the given point is greater than
         the starting point, and less than the ending point of the interval, as determined by the Start and End operators.
-        If precision is specified and the point type is a date/time type, comparisons used in the operation are performed
-            at the specified precision.
+        If precision is specified and the point type is a Date, DateTime, or Time type, comparisons used in the operation
+        are performed at the specified precision.
+        If the first argument is null, the result is null.
+        If the second argument is null the result is false.
 */
 
 import org.opencds.cqf.cql.engine.execution.State;
