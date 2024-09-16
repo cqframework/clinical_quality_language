aa17eaf04149dfe942fb8abf0e42df50fc9ba2b2
JP <jonathan.i.percival@gmail.com>
2024-09-16 15:26:07 -0600

    Remove usages of interfaces deprecated in latest FHIR core (#1414)
    * Remove usages of interfaces deprecated in latest FHIR core
    
    * Fix formatting

--- Src/java/cqf-fhir-npm/src/main/java/org/cqframework/fhir/npm/NpmPackageManager.java
+++ Src/java/cqf-fhir-npm/src/main/java/org/cqframework/fhir/npm/NpmPackageManager.java
@@ -4,7 +4,7 @@ import ca.uhn.fhir.model.primitive.IdDt;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.hl7.fhir.r5.context.IWorkerContext;
+import org.hl7.fhir.r5.context.ILoggingService;
 import org.hl7.fhir.r5.model.ImplementationGuide;
 import org.hl7.fhir.utilities.VersionUtilities;
 import org.hl7.fhir.utilities.npm.FilesystemPackageCacheManager;
@@ -12,7 +12,7 @@ import org.hl7.fhir.utilities.npm.NpmPackage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NpmPackageManager implements IWorkerContext.ILoggingService {
+public class NpmPackageManager implements ILoggingService {
     private static final Logger logger = LoggerFactory.getLogger(NpmPackageManager.class);
     private final ImplementationGuide sourceIg;
     private final FilesystemPackageCacheManager fspcm;
