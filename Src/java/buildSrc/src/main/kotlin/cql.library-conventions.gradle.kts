92631bc4381e8e9680e62ee6ed08666f48e196e9
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-09-17 12:57:01 -0600

    More tweaks to animalsniffer

--- Src/java/buildSrc/src/main/kotlin/cql.library-conventions.gradle.kts
+++ Src/java/buildSrc/src/main/kotlin/cql.library-conventions.gradle.kts
@@ -10,3 +10,7 @@ dependencies {
     // Various libraries for Android signatures are available, Jackson uses this one
     signature("com.toasttab.android:gummy-bears-api-${project.findProperty("android.api.level")}:0.5.0@signature")
 }
+
+tasks.animalsnifferTest {
+    enabled = false
+}
