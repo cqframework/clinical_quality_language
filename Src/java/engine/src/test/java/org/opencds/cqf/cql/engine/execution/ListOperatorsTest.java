8390d7af3e5d95849cd0f8ae77dbf1d935548f04
Bryn Rhodes <brynrhodes@users.noreply.github.com>
2024-11-05 13:49:57 -0600

    Improved generic instantiation behavior and added type precedence to … (#1428)
    * Improved generic instantiation behavior and added type precedence to ambiguous resolution logic
    
    * Fixes from regression testing
    
    * Spotless and quality gate fixes
    
    * Fixed precedence of infix set operators to match FHIRPath
    
    * Call isInstantiable recursively to make sure that type parameters (if present) are bound (#1431)
    
    * Removed unused typeMap field in InstantiationResult class
    
    * Spotless
    
    ---------
    
    Co-authored-by: Anton Vasetenkov <antvaset@gmail.com>

--- Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/execution/ListOperatorsTest.java
+++ Src/java/engine/src/test/java/org/opencds/cqf/cql/engine/execution/ListOperatorsTest.java
@@ -7,10 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opencds.cqf.cql.engine.elm.executing.EquivalentEvaluator.equivalent;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import org.cqframework.cql.cql2elm.CqlCompilerException;
 import org.cqframework.cql.cql2elm.CqlCompilerOptions;
 import org.junit.jupiter.api.Test;
