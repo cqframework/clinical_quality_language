b816b4b04f9ef1bd8881769b5667b579d4fc2c1e
Anton Vasetenkov <antvaset@gmail.com>
2024-11-06 09:52:12 +1300

    Remove redundant assertion

--- Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/SemanticTests.java
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/SemanticTests.java
@@ -382,12 +382,11 @@ public class SemanticTests {
 
     @Test
     void issue587() throws IOException {
-        CqlTranslator translator = TestUtils.runSemanticTest("Issue587.cql", 0);
-        // Both `collapse null` and `collapse { null }` now translate with no errors.
+        // Both `collapse null` and `collapse { null }` now translate with no errors (expectedErrors = 0).
         // The old errors were related to [#435](https://github.com/cqframework/clinical_quality_language/issues/435)
         // and fixed by [#1428](https://github.com/cqframework/clinical_quality_language/pull/1428) and
         // [#1425](https://github.com/cqframework/clinical_quality_language/pull/1425).
-        assertThat(translator.getErrors().size(), equalTo(1));
+        TestUtils.runSemanticTest("Issue587.cql", 0);
     }
 
     @Test
