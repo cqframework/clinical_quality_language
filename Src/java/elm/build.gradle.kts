6af4b689bf18bbe35abf82193e16fc3ba3745456
JP <jonathan.i.percival@gmail.com>
2024-11-06 08:55:37 -0700

    Swap Gradle language from Groovy to Kotlin (#1413)
    * WIP
    
    * WIP
    
    * WIP
    
    * Working?
    
    * Fix usage of Java 17 API
    
    * Update checkstyle rules
    
    * Fix static analysis
    
    * Updates to src dirs
    
    * Fixing up missing test sourceSet
    
    * Try another way to specify the antlr directory
    
    * Third different way
    
    * Add some logging
    
    * merge master
    
    * Change toolchain resolution
    
    * Fix tests
    
    * More tweaks to animalsniffer
    
    * Fix formatting
    
    * Trying random stuff
    
    * More random stuff
    
    * Small improvements for Gradle build (#1418)
    
    * Removing references to idea
    
    * Fix duplicative generation
    
    * Remove references to idea project generation
    
    ---------
    
    Co-authored-by: Anton Vasetenkov <antvaset@gmail.com>

---
+++ Src/java/elm/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("cql.library-conventions")
+    id("cql.xjc-conventions")
+}
+
+dependencies {
+    api(project(":model"))
+    testImplementation("org.jeasy:easy-random-core:5.0.0")
+    testImplementation("com.tngtech.archunit:archunit:1.2.1")
+}
+
+tasks.register<XjcTask>("generateAnnotation") {
+    schema = "${projectDir}/../../cql-lm/schema/elm/cqlannotations.xsd"
+    extraArgs = listOf("-npa")
+}
+
+tasks.register<XjcTask>("generateElm") {
+    schema = "${projectDir}/../../cql-lm/schema/elm/library.xsd"
+    extraArgs = listOf("-npa", "-XautoInheritance", "-XautoInheritance-xmlTypesExtend=org.cqframework.cql.elm.tracking.Trackable")
+}
