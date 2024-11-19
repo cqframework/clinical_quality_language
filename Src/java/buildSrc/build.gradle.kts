f7353473a03db000009a53439ee42aab319a005c
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-11-19 16:40:00 -0700

    Update Gradle to 8.11

--- Src/java/buildSrc/build.gradle.kts
+++ Src/java/buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.1.0")
-    implementation("ru.vyarus:gradle-animalsniffer-plugin:1.7.1")
+    implementation("ru.vyarus:gradle-animalsniffer-plugin:1.7.2")
     implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.0.14")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
 }
