55afde2443166a2ecf43c332b21b14232aef014b
Anton Vasetenkov <antvaset@gmail.com>
2024-11-20 13:57:04 +1300

    Fix error message

--- Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/invocation/InValueSetInvocation.java
+++ Src/java/cql-to-elm/src/main/java/org/cqframework/cql/cql2elm/model/invocation/InValueSetInvocation.java
@@ -23,7 +23,7 @@ public class InValueSetInvocation extends OperatorExpressionInvocation<InValueSe
     @Override
     public void setOperands(List<Expression> operands) {
         if (operands == null || operands.isEmpty() || operands.size() > 2) {
-            throw new IllegalArgumentException("InValueSet operation requires at one or two operands.");
+            throw new IllegalArgumentException("InValueSet operation requires one or two operands.");
         }
 
         expression.setCode(operands.get(0));
