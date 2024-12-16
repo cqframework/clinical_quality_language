3d54bb6e4a0f271a53d63d3b58c867e583cf5097
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-12-15 18:16:30 -0700

    Formatting

--- Src/java/engine-fhir/src/test/java/org/hl7/fhirpath/TestFhirPath.java
+++ Src/java/engine-fhir/src/test/java/org/hl7/fhirpath/TestFhirPath.java
@@ -10,7 +10,6 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.*;
-
 import org.cqframework.cql.cql2elm.tracking.Trackable;
 import org.fhir.ucum.UcumException;
 import org.hl7.elm.r1.Library;
