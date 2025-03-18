a0dcdcbf2fb51a06f73b23be0d87f1686a09b08f
Anton Vasetenkov <antvaset@gmail.com>
2025-03-19 08:50:29 +1300

    Bring back dependency to the signature configuration

--- Src/java/buildSrc/src/main/kotlin/cql.library-conventions.gradle.kts
+++ Src/java/buildSrc/src/main/kotlin/cql.library-conventions.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     id("cql.java-conventions")
 }
 
+dependencies {
+    // Various libraries for Android signatures are available, Jackson uses this one
+    signature("com.toasttab.android:gummy-bears-api-${project.findProperty("android.api.level")}:0.10.0:coreLib2@signature")
+}
+
 tasks.animalsnifferTest {
     enabled = false
 }
