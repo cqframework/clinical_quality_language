f72ee0f2c7d8d9202ae54ac3d0039545c8065145
Anton Vasetenkov <antvaset@gmail.com>
2025-04-28 20:02:51 +1200

    KDoc and visibility modifiers

--- Src/java/serialization/src/commonMain/kotlin/org/hl7/elm_modelinfo/r1/serializing/Json.kt
+++ Src/java/serialization/src/commonMain/kotlin/org/hl7/elm_modelinfo/r1/serializing/Json.kt
@@ -4,8 +4,15 @@ package org.hl7.elm_modelinfo.r1.serializing
 
 import org.cqframework.cql.elm.serializing.QName
 
-// The string can be formatted as "{namespaceURI}localPart" or just "localPart"
-fun jsonStringToQName(value: String): QName {
+/**
+ * Parses the qualified name from a JSON string. This is the reverse of the `QName.toString()`
+ * method.
+ *
+ * @param value The JSON string to parse, which may include a namespace URI, e.g.
+ *   "{urn:hl7-org:elm-types:r1}Integer".
+ * @return The corresponding `QName` object.
+ */
+internal fun jsonStringToQName(value: String): QName {
     if (value.startsWith("{")) {
         val parts = value.substring(1).split("}")
         return QName(parts[0], parts[1])
