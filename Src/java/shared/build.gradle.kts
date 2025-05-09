13def8530a2af8c54bb6e47e2d8176d480545393
Anton Vasetenkov <antvaset@gmail.com>
2025-05-09 18:31:52 +1200

    Fix sourceSets

--- Src/java/shared/build.gradle.kts
+++ Src/java/shared/build.gradle.kts
@@ -4,12 +4,6 @@ plugins {
 
 kotlin {
     sourceSets {
-        commonMain {
-            kotlin {
-                srcDir("build/generated/sources/cql/commonMain/kotlin")
-                srcDir("build/generated/sources/elm/commonMain/kotlin")
-            }
-        }
         jsMain {
             dependencies {
                 implementation("io.github.gciatto:kt-math-js:0.10.0")
