0bbc3b0d7efecec1fe0ee5efbba3484f5abc4745
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2025-06-04 15:10:40 +0200

    Improve documentation of Profile class

--- Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/Profile.java
+++ Src/java/engine/src/main/java/org/opencds/cqf/cql/engine/execution/Profile.java
@@ -16,14 +16,14 @@ import org.hl7.elm.r1.Retrieve;
  * <p>
  * Individual nodes thus correspond to expression or function
  * definitions being called in a particular context. Nodes track the
- * number of invocations, the time spent evaluating and the cache hit
- * ratio (if applicable) in that context. A profile is built by
- * creating and updating nodes whenever the engine finishes the
- * evaluation of a relevant element.
+ * number of invocations, the current CQL context, the time spent
+ * evaluating and the cache hit ratio (if applicable) in that
+ * context. A profile is built by creating and updating nodes whenever
+ * the engine finishes the evaluation of a relevant element.
  * <p>
  * Profiles for multiple evaluations can be merged into a combined
  * profile, for example to represent the performance characteristics
- * of use-cases that consists of multiple expression evaluation or the
+ * of use-cases that consists of multiple expression evaluations or to
  * form a combined picture of evaluations that were performed
  * concurrently in multiple engine instances.
  * <p>
@@ -33,13 +33,42 @@ import org.hl7.elm.r1.Retrieve;
  */
 public class Profile {
 
+    /**
+     * Represents all invocations of a particular CQL expression in a
+     * specific static context.
+     * <p>
+     * The static context consists of the chain of expression or
+     * function evaluations and the respective current CQL context
+     * that lead to the evaluation of the expression of the
+     * node. Another way to describe the static context is that it
+     * could be formed by aggregating call stacks in a way that
+     * disregards the concrete arguments of function calls but retains
+     * the current CQL context of each evaluation.
+     * <p>
+     * As a result, the parent of a node is a node that corresponds to
+     * the function or expression that called or used the expression
+     * of the node in question. The children of a node correspond to
+     * functions or expressions that are called or used by the
+     * expression of the node in question.
+     * <p>
+     * The information associated with these invocations is
+     * <ul>
+     *     <li>The CQL expression</li>
+     *     <li>The name of the CQL context, but not its value</li>
+     *     <li>The number of invocations</li>
+     *     <li>The cumulative real time spent in all invocations</li>
+     *     <li>The number of cache misses, if the expression is eligible for caching</li>
+     * </ul>
+     *
+     * @author Jan Moringen
+     */
     public static class Node {
         public Map<String, IdentityHashMap<Element, Node>> children = new HashMap<>();
         public Element expression;
         public String context;
         public long count = 0;
         public long time = 0;
-        private long misses = 0;
+        public long misses = 0;
 
         public Node(final Element expression, final String context) {
             this.expression = expression;
@@ -55,8 +84,9 @@ public class Profile {
         }
 
         public Node ensureChild(final Element expression, final String context) {
-            final var mapForContext = this.children.computeIfAbsent(context, context2 -> new IdentityHashMap<>());
-            return mapForContext.computeIfAbsent(expression, expression2 -> new Node(expression, context));
+            return this.children
+                    .computeIfAbsent(context, context2 -> new IdentityHashMap<>())
+                    .computeIfAbsent(expression, expression2 -> new Node(expression, context));
         }
 
         public Node register(final Collection<State.ActivationFrame> stack) {
@@ -132,7 +162,6 @@ public class Profile {
                     .flatMap(contextMap -> contextMap.values().stream())
                     .sorted(Comparator.comparing(node -> -node.time))
                     .collect(Collectors.toList());
-            var ic = i;
             var xc = x;
             for (var child : sorted) {
                 xc = child.renderStep(writer, i++, xc, depth + 1, scaleX, scaleY);
@@ -175,10 +204,40 @@ public class Profile {
 
     // Tracks the stack of nodes which correspond to currently active
     // ActivationFrames for incremental profile construction.
-    private final Deque<Node> stack = new ArrayDeque<Node>();
+    private final Deque<Node> stack = new ArrayDeque<>();
 
+    /**
+     * Creates a new, empty Profile.
+     * <p>
+     * Use methods for populating the profile.
+     *
+     * @see #enter Entering an activation frame for incremental profile construction
+     * @see #leave Leaving an activation frame for incremental profile construction
+     * @see #register Adding a snapshop of the engine stack to the profile
+     */
     public Profile() {}
 
+    /**
+     * Returns the tree of {@link Node}s of this profile.
+     *
+     * @return The root {@link Node} of this profile. Note that the
+     *         root node is not associated with a particular
+     *         expression or context; Both are {@code null}. The
+     *         direct children of the root node correspond to
+     *         top-level function calls or expression evaluations.
+     */
+    public Node getTree() {
+        return this.tree;
+    }
+
+    /**
+     * Records in the profile the fact that the evaluator has entered the supplied activation frame.
+     * <p>
+     * The activation frame must be fully populated in terms of element being evaluated, context, variables and evaluation start time. The end time has to be omitted, of course, since it becomes available when the evaluator leaves the activation frame.
+     * Each call of this method must be followed by a {@link #leave} call for the same activation frame.
+     *
+     * @param frame An activation frame that the evaluator has just entered or is about to enter.
+     */
     public void enter(final State.ActivationFrame frame) {
         final var topNode = this.stack.peek();
         final Node newNode;
@@ -192,50 +251,103 @@ public class Profile {
         this.stack.push(newNode);
     }
 
+    /**
+     * Records in the profile the fact that the evaluator has left the
+     * supplied activation frame.
+     * <p>
+     * The activation frame must be fully populated in terms of
+     * element being evaluated, context, variables and evaluation
+     * start and end time.  For each call of this method, there must
+     * be a preceding {@link #enter} call for the same activation
+     * frame.
+     *
+     * @param frame An activation frame that the evaluator has just
+     *              left or is about to leave.
+     */
     public void leave(final State.ActivationFrame frame) {
         final var topNode = this.stack.peek();
         assert topNode != null;
         assert topNode.expression == frame.element;
-        assert topNode.context.equals(frame.contextName);
+        assert Objects.equals(topNode.context, frame.contextName);
         topNode.addInvocation(frame.startTime, frame.endTime, frame.isCached);
         this.stack.pop();
     }
 
     /**
-     * Integrate a complete stack trace into the profile.
+     * Integrates a complete engine stack snapshot into the profile.
      *
-     * @see #enter for incrementally adding information from
-     *             individual activation frames.
-     * @see #leave for incrementally adding information from
-     *             individual activation frames.
+     * @see #enter Entering an activation frame for incremental profile construction
+     * @see #leave Leaving an activation frame for incremental profile construction
      */
     public void register(final Collection<State.ActivationFrame> stack) {
         this.tree.register(stack);
     }
 
+    /**
+     * Merges information from the other profile into this profile and
+     * returns the modified profile.
+     *
+     * @param other The other profile.
+     * @return This profile with the added information.
+     */
+    public Profile merge(final Profile other) {
+        this.tree.merge(other.tree);
+        return this;
+    }
+
+    /**
+     * Produces a new profile by merging the information in the
+     * provided collection of profiles.
+     *
+     * @param profiles A collection of profiles that should be merged.
+     * @return A new profile that contains all information from the
+     *         provided collection of profiles.
+     */
     public static Profile merge(final Collection<Profile> profiles) {
         final var result = new Profile();
         profiles.forEach(result::merge);
         return result;
     }
 
-    public Profile merge(final Profile other) {
-        this.tree.merge(other.tree);
-        return this;
-    }
-
+    /**
+     * Renders a flamegraph-SVG representation of the profile to the
+     * provided writer.
+     *
+     * @param writer A writer to which the flamegraph-SVG
+     *               representation will be written. This method does
+     *               not flush or close the writer.
+     * @throws IOException When writing to the writer throws an Exception.
+     */
     public void render(final Writer writer) throws IOException {
         writer.append("<svg>\n");
         this.tree.render(writer);
         writer.append("</svg>\n");
     }
 
+    /**
+     * Renders a flamegraph-SVG representation of the profile to the
+     * provided stream.
+     *
+     * @param outputStream A stream to which the flamegraph-SVG
+     *                     representation will be written. This method
+     *                     does not flush or close the stream.
+     * @throws IOException When writing to the stream throws an Exception.
+     */
     public void render(final OutputStream outputStream) throws IOException {
         try (var writer = new OutputStreamWriter(outputStream)) {
             render(writer);
         }
     }
 
+    /**
+     * Tries to create the designated file and write a flamegraph-SVG
+     * representation of the profile to it.
+     *
+     * @param outputFile The file to which the flamegraph-SVG
+     *                   representation should be written.
+     * @throws IOException If opening the file or writing to it throws
+     *                     an exception.
+     */
     public void render(final Path outputFile) throws IOException {
         try (var stream = new FileOutputStream(outputFile.toFile())) {
             render(stream);
