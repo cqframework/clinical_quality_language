d601d856518162889e891ec547419df89f8c8e64
Anton Vasetenkov <antvaset@gmail.com>
2025-09-11 21:32:39 +1200

    Update snapshot repository URL

--- Src/java/buildSrc/src/main/kotlin/cql.kotlin-multiplatform-conventions.gradle.kts
+++ Src/java/buildSrc/src/main/kotlin/cql.kotlin-multiplatform-conventions.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 repositories {
     mavenCentral()
     maven {
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 
