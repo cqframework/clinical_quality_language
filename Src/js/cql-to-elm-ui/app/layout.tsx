950e1e46384bef473f111f8dcac59259abeabc9e
Anton Vasetenkov <antvaset@gmail.com>
2025-10-07 22:17:28 +1300

    Update CQL to ELM UI colors and model URLs (#1622)

--- Src/js/cql-to-elm-ui/app/layout.tsx
+++ Src/js/cql-to-elm-ui/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "CQL Compiler in Kotlin/JS - Demo",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang={"en"}>
+    <html lang={"en"} className={roboto.className}>
       <body>{children}</body>
     </html>
   );
--- Src/js/cql-to-elm-ui/ui/button.ts
+++ Src/js/cql-to-elm-ui/ui/button.ts
@@ -1,10 +1,11 @@
 export const buttonStyle = {
   background: "transparent",
   boxShadow: "none",
-  color: "#444",
+  color: "inherit",
   border: "var(--border)",
   borderRadius: "var(--border-radius)",
   padding: "6px 12px",
   fontSize: "inherit",
   cursor: "pointer",
+  fontWeight: 700,
 };
