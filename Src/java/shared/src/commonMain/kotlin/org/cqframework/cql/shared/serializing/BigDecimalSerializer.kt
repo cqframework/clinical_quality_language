80b565698ab0e1701154c4e448898f862128a3ba
Anton Vasetenkov <antvaset@gmail.com>
2025-11-05 23:20:35 +1300

    Serialize BigDecimals as JSON strings in AST JSONs

--- Src/java/shared/src/commonMain/kotlin/org/cqframework/cql/shared/serializing/BigDecimalSerializer.kt
+++ Src/java/shared/src/commonMain/kotlin/org/cqframework/cql/shared/serializing/BigDecimalSerializer.kt
@@ -7,27 +7,19 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonEncoder
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.JsonUnquotedLiteral
 import org.cqframework.cql.shared.BigDecimal
 
-/** A JSON serializer for [BigDecimal] as a raw JSON number (unquoted literal). */
+/** A serializer for [BigDecimal] as a string. */
 object BigDecimalSerializer : KSerializer<BigDecimal> {
     override val descriptor: SerialDescriptor =
         PrimitiveSerialDescriptor("org.cqframework.cql.shared.BigDecimal", PrimitiveKind.STRING)
 
     @OptIn(ExperimentalSerializationApi::class)
     override fun serialize(encoder: Encoder, value: BigDecimal) {
-        require(encoder is JsonEncoder) { "BigDecimalSerializer can be used only with JSON format" }
-        encoder.encodeJsonElement(JsonUnquotedLiteral(value.toPlainString()))
+        encoder.encodeString(value.toPlainString())
     }
 
     override fun deserialize(decoder: Decoder): BigDecimal {
-        require(decoder is JsonDecoder) { "BigDecimalSerializer can be used only with JSON format" }
-        val jsonElement = decoder.decodeJsonElement()
-        require(jsonElement is JsonPrimitive) { "Expected JSON primitive for BigDecimal field" }
-        return BigDecimal(jsonElement.content)
+        return BigDecimal(decoder.decodeString())
     }
 }
