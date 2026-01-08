8b12a5c9093c9c6ee249c8821ad9034d1ece534b
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-01-08 11:44:45 +0100

    Add methods multiply, divideBy to UcumService interface
    Add implementations in createUcumService and DefaultUcumService.

--- Src/java/cql-to-elm/src/wasmJsMain/kotlin/org/cqframework/cql/cql2elm/ucum/UcumServiceWasmJs.kt
+++ Src/java/cql-to-elm/src/wasmJsMain/kotlin/org/cqframework/cql/cql2elm/ucum/UcumServiceWasmJs.kt
@@ -2,6 +2,8 @@
 
 package org.cqframework.cql.cql2elm.ucum
 
+import org.cqframework.cql.shared.BigDecimal
+
 actual val defaultLazyUcumService =
     lazy<UcumService> { error("No default UCUM service available.") }
 
@@ -10,6 +12,38 @@ actual val defaultLazyUcumService =
 fun createUcumServiceReference(
     convertUnit: (value: String, sourceUnit: String, destUnit: String) -> String,
     validateUnit: (unit: String) -> String?,
+    multiply:
+        (leftValue: String, leftUnit: String, rightValue: String, rightUnit: String) -> String,
+    divideBy: (leftValue: String, leftUnit: String, rightValue: String, rightUnit: String) -> String,
 ): JsReference<Lazy<UcumService>> {
-    return createUcumService(convertUnit, validateUnit).toJsReference()
+    return createUcumService(
+            convertUnit,
+            validateUnit,
+            { left, right ->
+                // The multiply function on the JS side has to encode the result as a string
+                // "<value>:<unit>" since we can
+                // pass and return only primitive values.
+                val result =
+                    multiply(
+                        left.first.toString(),
+                        left.second,
+                        right.first.toString(),
+                        right.second,
+                    )
+                val valueAndUnit = result.split(":", limit = 2)
+                Pair(BigDecimal(valueAndUnit[0]), valueAndUnit[1])
+            },
+            { left, right ->
+                val result =
+                    multiply(
+                        left.first.toString(),
+                        left.second,
+                        right.first.toString(),
+                        right.second,
+                    )
+                val valueAndUnit = result.split(":", limit = 2)
+                Pair(BigDecimal(valueAndUnit[0]), valueAndUnit[1])
+            },
+        )
+        .toJsReference()
 }
