43e94e68553ac00fb6cb48a5909d1f2279e9d758
raleigh-g-thompson <raleigh.g.thompson@gmail.com>
2026-05-29 16:19:12 -0600

    add support for breakpoint handling to the engine EvaulationVistitor

---
+++ Src/java/engine/src/commonMain/kotlin/org/opencds/cqf/cql/engine/debug/BreakpointHandler.kt
@@ -0,0 +1,13 @@
+package org.opencds.cqf.cql.engine.debug
+
+import org.hl7.elm.r1.Element
+import org.hl7.elm.r1.ExpressionDef
+import org.opencds.cqf.cql.engine.execution.State
+
+interface BreakpointHandler {
+    fun onBeforeExpression(elm: Element, state: State): BreakpointAction
+    fun onAfterExpression(elm: Element, state: State, value: Any?) {}
+    fun onExpressionDefEvaluated(elm: ExpressionDef, state: State, value: Any?) {}
+    fun waitForResume() {}
+    fun release() {}
+}
