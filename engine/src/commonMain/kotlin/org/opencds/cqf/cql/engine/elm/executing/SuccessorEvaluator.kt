159be56c1baa42b81d69db14fa5398684df6ec54
c-schuler <hoofschu@gmail.com>
2026-07-12 17:09:35 -0600

    Fix Successor/Predecessor of Date, DateTime, and Time to return null on overflow/underflow (#1734, #1733) (#1806)

--- engine/src/commonMain/kotlin/org/opencds/cqf/cql/engine/elm/executing/SuccessorEvaluator.kt
+++ engine/src/commonMain/kotlin/org/opencds/cqf/cql/engine/elm/executing/SuccessorEvaluator.kt
@@ -23,16 +23,22 @@ import org.opencds.cqf.cql.engine.runtime.toCqlLong
 successor of<T>(argument T) T
 
 The successor operator returns the successor of the argument. For example, the successor of 1 is 2.
-  If the argument is already the maximum value for the type, a run-time error is thrown.
+  If the argument is already the maximum value for the type, or the result cannot otherwise be
+  represented (i.e. would result in overflow), the result is null.
 The successor operator is defined for the Integer, Long, Decimal, DateTime, and Time types.
 For Integer, Long successor is equivalent to adding 1.
 For Decimal, successor is equivalent to adding the minimum precision value for the Decimal type, or 10^-08.
-For DateTime and Time values, successor is equivalent to adding a time-unit quantity for the lowest specified precision of the value.
+For DateTime and Time values, successor is equivalent to adding a time-unit quantity for the
+  lowest specified precision of the value.
   For example, if the DateTime is fully specified, successor is equivalent to adding 1 millisecond;
     if the DateTime is specified to the second, successor is equivalent to adding one second, etc.
 If the argument is null, the result is null.
 */
 object SuccessorEvaluator {
+    // The maximum representable year for Date/DateTime values (see the bounds enforced by the
+    // Date and DateTime types).
+    private const val MAX_YEAR = 9999
+
     /**
      * Checks if the given BigDecimal value exceeds the maximum allowed value for Decimal type.
      *
@@ -82,53 +88,37 @@ object SuccessorEvaluator {
                 .withValue((successor(quantity.value?.toCqlDecimal()) as Decimal).value)
                 .withUnit(quantity.unit)
         } else if (value is Date) {
-            val dt = value
-            return Date(dt.date!!.plus(1, dt.precision!!.toChronoUnit()), dt.precision!!)
+            val next = value.date!!.plus(1, value.precision!!.toChronoUnit())
+            // If incrementing overflows the representable year range, the result is null.
+            return if (next.getYear() > MAX_YEAR) null else Date(next, value.precision!!)
         } else if (value is DateTime) {
-            val dt = value
-            return DateTime(dt.dateTime!!.plus(1, dt.precision!!.toChronoUnit()), dt.precision!!)
+            val next = value.dateTime!!.plus(1, value.precision!!.toChronoUnit())
+            // If incrementing overflows the representable year range, the result is null.
+            return if (next.getYear() > MAX_YEAR) null else DateTime(next, value.precision!!)
         } else if (value is Time) {
             val t = value
-            when (t.precision!!) {
-                Precision.HOUR ->
-                    if (t.time.getHour() == 23) {
-                        throw TypeOverflow(
-                            "The result of the successor operation exceeds the maximum value allowed for the Time type"
-                        )
-                    }
-                Precision.MINUTE ->
-                    if (t.time.getHour() == 23 && t.time.getMinute() == 23) {
-                        throw TypeOverflow(
-                            "The result of the successor operation exceeds the maximum value allowed for the Time type"
-                        )
-                    }
-                Precision.SECOND ->
-                    if (
+            // If the value is already the maximum for its precision, incrementing overflows the
+            // representable range and the result is null.
+            val overflow =
+                when (t.precision!!) {
+                    Precision.HOUR -> t.time.getHour() == 23
+                    Precision.MINUTE -> t.time.getHour() == 23 && t.time.getMinute() == 59
+                    Precision.SECOND ->
                         t.time.getHour() == 23 &&
-                            t.time.getMinute() == 23 &&
+                            t.time.getMinute() == 59 &&
                             t.time.getSecond() == 59
-                    ) {
-                        throw TypeOverflow(
-                            "The result of the successor operation exceeds the maximum value allowed for the Time type"
-                        )
-                    }
-                Precision.MILLISECOND ->
-                    if (
+                    Precision.MILLISECOND ->
                         t.time.getHour() == 23 &&
                             t.time.getMinute() == 59 &&
                             t.time.getSecond() == 59 &&
                             t.time.get(Precision.MILLISECOND.toChronoField()) == 999
-                    ) {
-                        throw TypeOverflow(
-                            "The result of the successor operation exceeds the maximum value allowed for the Time type"
-                        )
-                    }
-                Precision.DAY,
-                Precision.MONTH,
-                Precision.WEEK,
-                Precision.YEAR -> {}
-            }
-            return Time(t.time.plus(1, t.precision!!.toChronoUnit()), t.precision!!)
+                    Precision.DAY,
+                    Precision.MONTH,
+                    Precision.WEEK,
+                    Precision.YEAR -> false
+                }
+            return if (overflow) null
+            else Time(t.time.plus(1, t.precision!!.toChronoUnit()), t.precision!!)
         }
 
         throw InvalidOperatorArgument(
