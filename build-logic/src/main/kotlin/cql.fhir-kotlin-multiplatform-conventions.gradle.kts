f47daa2e851057a78a61d23b6c2dddbeaf16047b
Anton Vasetenkov <antvaset@gmail.com>
2026-07-18 11:43:06 +1200

    Rework shared xjc and fhir plugins

--- build-logic/src/main/kotlin/cql.fhir-kotlin-multiplatform-conventions.gradle.kts
+++ build-logic/src/main/kotlin/cql.fhir-kotlin-multiplatform-conventions.gradle.kts
@@ -1,15 +1,6 @@
-plugins { id("cql.kotlin-multiplatform-conventions") }
-
-// bug fix for the hapi-bom
-configurations.all {
-    resolutionStrategy {
-        eachDependency {
-            if (requested.group == "org.eclipse.jetty") {
-                useVersion("11.0.20")
-                because("jetty 12 is java 17")
-            }
-        }
-    }
+plugins {
+    id("cql.kotlin-multiplatform-conventions")
+    id("cql.fhir-common-conventions")
 }
 
 kotlin {
