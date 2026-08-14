ecc8e778437daaa6dc482bdfb138b13930220c73
Anton Vasetenkov <antvaset@gmail.com>
2026-08-14 12:37:53 +1200

    Update ANTLR Kotlin runtime

--- cql/build.gradle.kts
+++ cql/build.gradle.kts
@@ -48,10 +48,10 @@ kotlin {
             }
             dependencies {
                 api(project(":shared"))
-                api("com.strumenta:antlr-kotlin-runtime:1.0.3")
+                api("com.strumenta:antlr-kotlin-runtime:1.0.12")
             }
         }
-        jvmMain { dependencies { api("com.strumenta:antlr-kotlin-runtime-jvm:1.0.3") } }
+        jvmMain { dependencies { api("com.strumenta:antlr-kotlin-runtime-jvm:1.0.12") } }
         jvmTest {
             dependencies {
                 implementation(project(":quick"))
