093cd8709c33ad0ee169d93966524fadae50c12b
c-schuler <hoofschu@gmail.com>
2026-09-04 15:51:48 -0600

    Make createIdentityHashMap an identity map on JS and wasm

--- engine/src/wasmJsMain/kotlin/org/opencds/cqf/cql/engine/util/IdentityHashMap.kt
+++ engine/src/wasmJsMain/kotlin/org/opencds/cqf/cql/engine/util/IdentityHashMap.kt
@@ -1,5 +1,10 @@
 package org.opencds.cqf.cql.engine.util
 
+/**
+ * [mutableMapOf] would compare keys with [equals], which is the opposite of what an identity map
+ * promises: two structurally equal but distinct objects would collapse into one entry. See
+ * [IdentityKeyedMap].
+ */
 actual fun <K, V> createIdentityHashMap(): MutableMap<K, V> {
-    return mutableMapOf()
+    return IdentityKeyedMap()
 }
